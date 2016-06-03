d08ce3939cac6eed391c678b9aad0dadf04790b8
Jose Pena <josepena@MacBook-Pro-de-Jose.local>
2016-06-03 17:40:15 -0400

    fixing output

--- launch.c
+++ launch.c
@@ -13,61 +13,35 @@
  	int lowest_temp;
  	char above_99;
  	char raining;
- 	int errors; 
+
 
  	/* User Input */
- 	errors = 0;
  	printf("\n\nWhat is the average temperature?\n\n");
  	scanf("%d", &avg_temp);
- 	if (avg_temp < 41) {
- 		errors++; 
- 	}
 
  	printf("What is the lowest temperature in last 24 hours?\n\n");
  	scanf("%d", &lowest_temp);
- 	if (lowest_temp < 32){
- 		errors++;
- 	}
 
  	printf("Has the temperature been over 99°F for more than");
  	printf(" 30 minutes? Please answer Y for yes and N for no\n\n");
  	scanf(" %c", &above_99);
- 	if (above_99 != 'N' && above_99 != 'n'){
- 		errors++;
- 	}
 
  	printf("Is it raining currently? Please answer Y for yes and N for no\n\n");
  	scanf(" %c", &raining);
- 	if (raining != 'N' && raining != 'n'){
- 		errors++;
- 	}
+
 
  	/* Program Output */
- 	if (errors == 0) {
+ 	if (avg_temp > 41 && lowest_temp > 32 && (above_99 == 'N' || 
+ 		above_99 == 'n') && (raining == 'N' || raining == 'n')) {
  		printf("\n\nIt is a go. The shuttle will be launched as scheduled.\n\n");
  	}
- 	else if (errors == 1) {
+ 	else if (avg_temp <= 41) {
  		printf("\n\nThe shuttle launch has been cancelled because");
- 		if (avg_temp < 41){
- 			printf(" the average temperature is below 41 degrees\n");
- 		}
- 		else if(lowest_temp < 32){
- 			printf(" the temperature has dropped below freezing in the last 24 hours\n");
- 		}
- 		else if(above_99 != 'N' && above_99 != 'n') {
- 			printf(" the temperature has been over 99°F for more than 30 minutes\n");
- 		}
- 		else {
- 			printf(" it is raining\n");
- 		}
-
+ 		printf(" the average temperature is below 41 degrees\n");
  	}
  	else {
  		printf("\n\nThe shuttle launch has been cancelled because:\n\n");
- 		if (avg_temp < 41){
- 			printf("The average temperature is below 41 degrees\n\n");
- 		}
- 		if(lowest_temp < 32){
+ 		if(lowest_temp <= 32){
  			printf("The temperature has dropped below freezing in the last 24 hours\n\n");
  		}
  		if(above_99 != 'N' && above_99 != 'n') {
